16547ff96d2dc148147f83005a26c1d7cc017ebd
dalburez <gatemonte@gmail.com>
2018-06-02 10:54:29 +0200

    f

--- code/app.R
+++ code/app.R
@@ -8,8 +8,6 @@ library(gender)
 library(shiny)
 
 author_gender <- function(bib, api = "https://api.crossref.org/works?query.bibliographic=", ...) {
-  # browser()
-  # print(bib)
   
   bib_e <- URLencode(bib, reserved = T)
   
@@ -31,95 +29,78 @@ author_gender <- function(bib, api = "https://api.crossref.org/works?query.bibli
   auth_df$name <- sapply(auth_df$name, function(x) unlist(strsplit(x, "[[:space:]]"))[1] )
   auth_df$name <- gsub('[[:punct:] ]+',' ',auth_df$name)
   
-  # gen_df <- 
-  
   out <- gender_df(auth_df, name_col = "name", year_col = "year")
   
   if(nrow(out)>0) {
     out <- out[na.omit(match(auth_df$name, out$name)), ] # Order according to authorship
     out$order <- 1:nrow(out)
+    out$is_last <- F # Assign last
+    out[nrow(out),'is_last'] <- T
   } 
   
   return(out)
 }
 
+# UI ####
+
 ui <- fluidPage(
   textAreaInput("bib", "", width = "700px", height = "300px", placeholder = "Paste your bibliography here"),
-  # textInput("bib", "", placeholder = "Paste your bibliography here"),
-  
-  tabPanel(
-    "2 columns",
-    fluidRow(
-      column(width = 4,
-             h2("Gender of first author"),
-             tableOutput("first")
-             ),
-      column(width = 4,
-             h2("Gender of all authors"),
-             tableOutput("all")
-      )    )
-  )
+  tableOutput("table")
 )
+
+# Server ####
 
 server <- function(input, output) {
   
-  first <- eventReactive(input$bib, {
+  tab <- eventReactive(input$bib, {
     
     if(input$bib != "") {
-      # browser()
+      
       bib <- unlist(strsplit(input$bib, "\n"))
       res_df <- do.call(rbind.data.frame, lapply(bib, author_gender) ) 
       
-      res_df %>% 
-        dplyr::filter(order == 1) %>% 
-        dplyr::rename(Gender = gender) %>% 
-        dplyr::group_by(Gender) %>% 
-        dplyr::summarise(
-          `No. authors` = n(),
-          Share = round(n()/nrow(dplyr::filter(res_df, order == 1))*100, 1)
-        )
+      # All authors
+      
+      df <- res_df
+      
+      no_all <- no_authors <- nrow(df)
+      
+      fem_all <- length(df$gender[df$gender == "female"])
+      
+      share_all <- round(fem_all/no_authors*100, digits = 1)
+      
+      # First author
+      
+      df <- res_df %>% dplyr::filter(order == 1)
+      
+      no_first <- no_authors <- nrow(df)
+      
+      fem_first <- length(df$gender[df$gender == "female"])
+      
+      share_first <- round(fem_first/no_authors*100, digits = 1)
+      
+      # Last author
+      
+      df <- res_df %>% dplyr::filter(is_last)
+      
+      no_last <- no_authors <- nrow(df)
+      
+      fem_last <- length(df$gender[df$gender == "female"])
+      
+      share_last <- round(fem_last/no_authors*100, digits = 1)
+      
+      data.frame(`Type of author` = c("All authors", "First author only", "Last author only"), 
+                 `Number of authors identified` = c(no_all, no_first, no_last),
+                 `Number of female authors` = c(fem_all, fem_first, fem_last),
+                 `Percent of female authors` = c(share_all, share_first, share_last),
+                 check.names = FALSE)
       
     } else {
       data.frame(Status = "Waiting for entry")
     }
-    
   })
   
-  all <- eventReactive(input$bib, {
-    
-    if(input$bib != "") {
-      # browser()
-      bib <- unlist(strsplit(input$bib, "\n"))
-      res_df <- do.call(rbind.data.frame, lapply(bib, author_gender) ) 
-      
-      res_df %>% 
-        # dplyr::filter(order == 1) %>% 
-        dplyr::rename(Gender = gender) %>% 
-        dplyr::group_by(Gender) %>% 
-        dplyr::summarise(
-          `No. authors` = n(),
-          Share = round(n()/nrow(res_df)*100, 1)
-        )
-      
-    } else {
-      data.frame(Status = "Waiting for entry")
-    }
-    
-  })
-  
-  
-  
-  output$first <- renderTable({
-    
-    first()
-
-  })
-  
-  output$all <- renderTable({
-    
-    all()
-    
-  })
+  output$table <- renderTable({ tab() })
   
 }
 
